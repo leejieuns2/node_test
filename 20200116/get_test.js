7ec5087cd17f5f87059de0f49cb8714ccecd560e
leejieuns2 <2jjingni@gmail.com>
2020-01-16 15:30:22 +0900

    add explain

--- 20200116/get_test.js
+++ 20200116/get_test.js
@@ -1,3 +1,10 @@
+/* RESTful API 
+ * 쓰기 : Create POST
+ * 읽기 : Read GET
+ * 수정 : Update PUT
+ * 삭제 : Delete DEL
+ */
+
 var http = require('http');
 var url = require('url');
 var querystring = require('querystring');
@@ -6,7 +13,8 @@ http.createServer(function (request, response) {
     var query = url.parse(request.url, true).query;
 
     response.writeHead(200, {'Content-Type': 'text/html'});
-    response.end('<h1>' + JSON.stringify(query) + '</h1');
+    response.end('<h1>' + JSON.stringify(query) + '</h1>');
+    // JSON 사용해서 Query를 확인할 수 있음
 }).listen(1234, function() {
     console.log('Server running at http://127.0.0.1:1234');
 });
